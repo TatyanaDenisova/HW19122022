bf16cb104aaf296998bace852f142a1acfc08ec8
Tatiana Denisova <2746619@gmail.com>
2022-12-24 06:39:13 +0300

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+int b = 0;
+
+while(a<99999)
+{
+if(a <= 99)
+{
+  Console.WriteLine("третьей цифры нет");
+  return;
+}
+if(a<=999)
+{
+  b = a % 10;
+  Console.WriteLine(b);
+  return;
+}
+if(a<=9999)
+{
+  b = a/10 % 10; 
+  Console.WriteLine(b); 
+  return;
+}
+if(a<=99999)
+{
+  b = a/100 % 10; 
+  Console.WriteLine(b); 
+  return;
+}
+}
